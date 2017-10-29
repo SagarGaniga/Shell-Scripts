e68ebaecbadd3f3d7501cca8d220f413e148eb2b
sagarganiga <sagarganiga468@gmail.com>
2017-10-29 16:56:24 +0530

    Add pattern1234.sh

---
+++ pattern_1234.sh
@@ -0,0 +1,24 @@
+#Echo for printing on the console
+echo "Enter the length of triangle: "
+
+#read input from user
+read len
+
+#outer for loop
+
+#loop runs from 1 to user specified length
+#Efficient way to do it is to use the ksh for (( )) style of loop 
+#Also supported by bash and zsh for decades.
+
+for ((i = 1; i <= len; i++)); 
+do
+	#Inner for loop
+	#prints 1 to i
+	for ((j = 1; j <= $i; j++));
+	do
+    	echo -n "$j"
+	done
+
+	#Enter a new line
+	echo ""
+done
