dec59cd0ffcdab9a3b9c4bd628a3ec3f54fc9dae
sagarganiga <sagarganiga468@gmail.com>
2017-10-29 19:34:04 +0530

    add few more files

--- pattern_1234.sh
+++ pattern_1234.sh
@@ -5,7 +5,6 @@ echo "Enter the length of triangle: "
 read len
 
 #outer for loop
-
 #loop runs from 1 to user specified length
 #Efficient way to do it is to use the ksh for (( )) style of loop 
 #Also supported by bash and zsh for decades.
@@ -14,9 +13,9 @@ for ((i = 1; i <= len; i++));
 do
 	#Inner for loop
 	#prints 1 to i
-	for ((j = 1; j <= $i; j++));
+	for ((j = 1; j <= i; j++));
 	do
-    	echo -n "$j"
+    	echo -n "$j"	# -n: do not output the trailing newline
 	done
 
 	#Enter a new line
